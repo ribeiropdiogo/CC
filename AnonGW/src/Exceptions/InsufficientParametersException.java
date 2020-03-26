2ba99022c0c85ba22d012b9edded02523253ed56
Diogo Ribeiro <ribeiropdiogo@gmail.com>
2020-03-26 08:55:56 +0000

    compor pastas

---
+++ AnonGW/src/Exceptions/InsufficientParametersException.java
@@ -0,0 +1,7 @@
+package Exceptions;
+
+public class InsufficientParametersException extends Exception {
+    public InsufficientParametersException(String message){
+        super(message);
+    }
+}
---
+++ AnonGW/src/Request.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+
+public class Request {
+    private long creationTime;
+    private String origin_address;
+    private String message;
+    private List<String> response;
+    private String status; // na -> não atendido / ad -> atendido no destino / sd -> servido no destino / to -> a ser transmitido à origem / so -> servido na origem / tbd -> to be deleted
+
+    public Request(String address){
+        this.creationTime = System.currentTimeMillis();
+        this.origin_address = address;
+        this.status = "na";
+        this.response = new ArrayList();
+    }
+
+    public String getOrigin_address() {
+        return origin_address;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public long getCreationTime(){
+        return this.creationTime;
+    }
+
+    public synchronized void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void concatenateResponse(String message){
+        String s = message + "\r\n";
+        this.response.add(s);
+    }
+
+    public Object[] getResponse(){
+        return this.response.toArray();
+    }
+
+    public synchronized String getStatus() {
+        return this.status;
+    }
+
+    public synchronized void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Request request = (Request) o;
+        return Objects.equals(origin_address, request.origin_address) &&
+                Objects.equals(message, request.message);
+    }
+
+    public void printRequest(){
+        System.out.println("Request: ");
+        System.out.println("Source: " + this.origin_address);
+        System.out.println("Message: " + this.message);
+        System.out.println("Creation Time: " + this.creationTime);
+        System.out.println("Status: " + this.status);
+    }
+}
