1dea395d9b85f2789a2b65ef1262f5df23472371
Diogo Ribeiro <ribeiropdiogo@gmail.com>
2020-05-16 22:06:02 +0100

    fix

--- src/NodeUDPListener.java
+++ src/NodeUDPListener.java
@@ -149,12 +149,6 @@ public class NodeUDPListener implements Runnable{
                 this.suspects.add(id);
                 startPDUChecker(id);
             }
-            /*
-            int[] missingP = null;
-            checkMissingPositions(id,missingP);
-            sendControlPackage(id,missingP);
-
-             */
         }
     }
 
@@ -182,6 +176,8 @@ public class NodeUDPListener implements Runnable{
                     System.out.println("adasdasd");
                 else if(!served.contains(pdu.getIdentifier(secretKey)))
                     System.out.println("ssssssssss");
+
+                System.out.println(pdu.getIdentifier(secretKey));
 
                 if (validOrigin(pdu.getIdentifier(secretKey)) && !served.contains(pdu.getIdentifier(secretKey))) {
                     System.out.println("> UDPListener: Packet Received");
